757656f801bbd9625ee30984d4b8ce6a07b6d981
Mats Larsen <me@supergrecko.com>
2022-01-30 15:25:43 +0100

    Set up GraphQL server with gqlgen

--- bin/main.go
+++ bin/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/dotkom/a3s/graph"
+	"github.com/dotkom/a3s/graph/generated"
+	"github.com/go-chi/chi/v5"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	server := chi.NewRouter()
+	config := generated.Config{Resolvers: &graph.Resolver{}}
+	schema := generated.NewExecutableSchema(config)
+	srv := handler.NewDefaultServer(schema)
+
+	server.Get("/", srv.ServeHTTP)
+	server.Get("/playground", playground.Handler("GraphQL Playground", "/"))
+
+	log.Println("started GraphQL server at http://localhost:8000/playground")
+	err := http.ListenAndServe(":8000", server)
+	if err != nil {
+		log.Fatalf("failed to start server %s\n", err)
+	}
 }
