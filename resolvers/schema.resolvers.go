ff86828ed24817691f17e9f88d48b584ade2e035
magnusrodseth <59113973+magnusrodseth@users.noreply.github.com>
2022-02-20 15:25:59 +0100

    refactor: Split resolvers

--- resolvers/schema.resolvers.go
+++ resolvers/schema.resolvers.go
@@ -6,82 +6,24 @@ package resolvers
 import (
 	"context"
 
-	"github.com/dotkom/a3s/ent/company"
-	"github.com/dotkom/a3s/ent/eventorganizer"
 	"github.com/dotkom/a3s/graph/generated"
 	"github.com/dotkom/a3s/graph/model"
 )
 
 func (r *queryResolver) Companies(ctx context.Context) ([]*model.Company, error) {
-	background := context.Background()
-	all, err := r.Client.Company.Query().All(background)
-	if err != nil {
-		return nil, err
-	}
-	var list []*model.Company
-	for _, result := range all {
-		record := model.Company{
-			ID:               result.ID,
-			Name:             result.Name,
-			ShortDescription: result.ShortDescription,
-			LongDescription:  result.LongDescription,
-			ImageURL:         result.ImageURL,
-			Site:             &result.Site,
-			ContactEmail:     &result.ContactEmail,
-			ContactTel:       &result.ContactTel,
-		}
-		list = append(list, &record)
-	}
-	return list, nil
+	return Companies(r, ctx)
 }
 
 func (r *queryResolver) Company(ctx context.Context, name string) (*model.Company, error) {
-	background := context.Background()
-	result, err := r.Client.Company.Query().Where(company.NameEQ(name)).Only(background)
-	if err != nil {
-		return nil, err
-	}
-	return &model.Company{
-		ID:               result.ID,
-		Name:             result.Name,
-		ShortDescription: result.ShortDescription,
-		LongDescription:  result.LongDescription,
-		ImageURL:         result.ImageURL,
-		Site:             &result.Site,
-		ContactEmail:     &result.ContactEmail,
-		ContactTel:       &result.ContactTel,
-	}, nil
+	return Company(r, ctx, name)
 }
 
 func (r *queryResolver) EventOrganizers(ctx context.Context) ([]*model.EventOrganizer, error) {
-	background := context.Background()
-	all, err := r.Client.EventOrganizer.Query().All(background)
-	if err != nil {
-		return nil, err
-	}
-	var list []*model.EventOrganizer
-	for _, result := range all {
-		record := model.EventOrganizer{
-			ID:    result.ID,
-			Name:  result.Name,
-			Email: result.Email,
-		}
-		list = append(list, &record)
-	}
-	return list, nil
+	return EventOrganizers(r, ctx)
 }
 
 func (r *queryResolver) EventOrganizer(ctx context.Context, name string) (*model.EventOrganizer, error) {
-	background := context.Background()
-	result, err := r.Client.EventOrganizer.Query().Where(eventorganizer.NameEQ(name)).Only(background)
-	if err != nil {
-		return nil, err
-	}
-	return &model.EventOrganizer{
-		ID:    result.ID,
-		Name:  result.Name,
-		Email: result.Email,
-	}, nil
+	return EventOrganizer(r, ctx, name)
 }
 
 // Query returns generated.QueryResolver implementation.
