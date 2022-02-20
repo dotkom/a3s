84a3d6741b131f23e0184d7d0c687e411332ea92
magnusrodseth <59113973+magnusrodseth@users.noreply.github.com>
2022-02-20 14:10:12 +0100

    feat: Add event_organizer entity and corresponding database schema

--- resolvers/schema.resolvers.go
+++ resolvers/schema.resolvers.go
@@ -7,6 +7,7 @@ import (
 	"context"
 
 	"github.com/dotkom/a3s/ent/company"
+	"github.com/dotkom/a3s/ent/eventorganizer"
 	"github.com/dotkom/a3s/graph/generated"
 	"github.com/dotkom/a3s/graph/model"
 )
@@ -49,6 +50,37 @@ func (r *queryResolver) Company(ctx context.Context, name string) (*model.Compan
 		Site:             &result.Site,
 		ContactEmail:     &result.ContactEmail,
 		ContactTel:       &result.ContactTel,
+	}, nil
+}
+
+func (r *queryResolver) EventOrganizers(ctx context.Context) ([]*model.EventOrganizer, error) {
+	background := context.Background()
+	all, err := r.Client.EventOrganizer.Query().All(background)
+	if err != nil {
+		return nil, err
+	}
+	var list []*model.EventOrganizer
+	for _, result := range all {
+		record := model.EventOrganizer{
+			ID:    result.ID,
+			Name:  result.Name,
+			Email: result.Email,
+		}
+		list = append(list, &record)
+	}
+	return list, nil
+}
+
+func (r *queryResolver) EventOrganizer(ctx context.Context, name string) (*model.EventOrganizer, error) {
+	background := context.Background()
+	result, err := r.Client.EventOrganizer.Query().Where(eventorganizer.NameEQ(name)).Only(background)
+	if err != nil {
+		return nil, err
+	}
+	return &model.EventOrganizer{
+		ID:    result.ID,
+		Name:  result.Name,
+		Email: result.Email,
 	}, nil
 }
 
