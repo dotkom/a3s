d47b47392d565c7099b8ab9190f4bba5c9468ab9
magnusrodseth <59113973+magnusrodseth@users.noreply.github.com>
2022-02-27 14:45:56 +0100

    Add relations for RuleBundle and Extras

--- ent/schema/rulebundle.go
+++ ent/schema/rulebundle.go
@@ -2,6 +2,7 @@ package schema
 
 import (
 	"entgo.io/ent"
+	"entgo.io/ent/schema/edge"
 	"entgo.io/ent/schema/field"
 )
 
@@ -19,5 +20,7 @@ func (RuleBundle) Fields() []ent.Field {
 
 // Edges of the RuleBundle.
 func (RuleBundle) Edges() []ent.Edge {
-	return nil
+	return []ent.Edge{
+		edge.From("registrations", Registration.Type).Ref("rule_bundles"),
+	}
 }
